c0126c76f34830596dd398fc32578c38614d923a
Kumar Dhanagopal <kumar.dhanagopal@oracle.com>
2019-10-11 12:54:09 -0700

    Update tesseract.template.sh

--- modules/bastion/scripts/tesseract.template.sh
+++ modules/bastion/scripts/tesseract.template.sh
@@ -3,4 +3,4 @@
 # Copyright 2017, 2019, Oracle Corporation and/or affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl
 
-ssh -i ${user}@${bastion_ip}
+ssh ${user}@${bastion_ip}
